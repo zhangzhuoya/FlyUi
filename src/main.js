b45a2c7fcae054f4aea349a107181c0c8a583f7f
zhangzhuoya <1114976368@qq.com>
2021-09-09 15:33:02 +0800

    添加测试用例 不完整版

--- src/main.js
+++ src/main.js
@@ -3,7 +3,48 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 import Button from "./demo/button.vue";
+import chai from "chai";
+import spies from 'chai-spies';
+chai.use(spies)
 Vue.component("g-button", Button);
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
+console.log(chai);
+const expect = chai.expect;
+// 单元测试
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: "settings",
+    },
+  });
+  vm.$mount(); // 挂载到内存
+  let useElement = vm.$el.querySelector("use");
+  let href = useElement.getAttribute("xlink:href");
+  expect(href).to.eq("#icon-settings");
+  vm.$el.remove();
+  vm.$destroy();
+}
+// {
+//   const Constructor = Vue.extend(Button);
+//   const vm = new Constructor({
+//     propsData: {
+//       icon:"loading"
+//     }
+//   })
+//   vm.$mount();
+//   let use
+// }
+{
+  // mock: 函数的mock
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor();
+  vm.$mount();
+  let spy = chai.spy(function () {});
+  vm.$on("click", spy);
+  let button = vm.$el;
+  button.click();
+  expect(spy).to.have.been.called();
+}
