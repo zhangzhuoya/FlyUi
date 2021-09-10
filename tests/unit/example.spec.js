7ff0cd49e2850b10b009e96a76920402264c6a7a
zhangzhuoya <1114976368@qq.com>
2021-09-10 10:14:56 +0800

    describe的标题必须为字符串形式

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -5,34 +5,58 @@ import Button from "@/demo/button.vue";
 chai.use(spies);
 const expect = chai.expect;
 import Vue from "vue";
-{
-  const Constructor = Vue.extend(Button);
-  const vm = new Constructor({
-    propsData: {
-      icon: "settings",
-    },
+describe("Button", () => {
+  // BDD 行为测试
+  it("存在", () => {
+    expect(Button).to.be.ok;
   });
-  vm.$mount(); // 挂载到内存
-  let useElement = vm.$el.querySelector("use");
-  let href = useElement.getAttribute("xlink:href");
-  expect(href).to.eq("#icon-settings");
-  vm.$el.remove();
-  vm.$destroy();
-}
-{
-  const Constructor = Vue.extend(Button);
-  const vm = new Constructor({
-    propsData: {
-      icon: "loading",
-    },
+
+  it("可以设置icon", () => {
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+      propsData: {
+        icon: "settings",
+      },
+    });
+    vm.$mount(); // 挂载到内存
+    let useElement = vm.$el.querySelector("use");
+    let href = useElement.getAttribute("xlink:href");
+    expect(href).to.equal("#icon-settings");
+    vm.$el.remove();
+    vm.$destroy();
   });
-  vm.$mount();
-  let useElement = vm.$el.querySelector("use");
-  let href = useElement.getAttribute("xlink:href");
-  expect(href).to.eq("#icon-loading");
-  vm.$el.remove();
-  vm.$destroy();
-}
+  it("可以设置loading", () => {
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+      propsData: {
+        icon: "loading",
+      },
+    });
+    vm.$mount();
+    let useElement = vm.$el.querySelector("use");
+    let href = useElement.getAttribute("xlink:href");
+    expect(href).to.equal("#icon-loading");
+    vm.$el.remove();
+    vm.$destroy();
+  });
+  // it("可以设置icon位置", () => {
+  //   const div = document.createElement("div");
+  //   document.body.appendChild(div);
+  //   const Constructor = Vue.extend(Button);
+  //   const vm = new Constructor({
+  //     propsData: {
+  //       icon: "settings",
+  //     },
+  //   });
+  //   vm.$mount(div);
+  //   let svg = vm.$el.querySelector("svg");
+  //   console.log(window.getComputedStyle(svg).order, "111111111");
+  //   let { order } = window.getComputedStyle(svg);
+  //   expect(order).to.eq("");
+  //   vm.$el.remove();
+  //   vm.$destroy();
+  // });
+});
 // {
 //   const div = document.createElement("div");
 //   document.body.appendChild(div);
@@ -50,19 +74,18 @@ import Vue from "vue";
 //   vm.$el.remove();
 //   vm.$destroy();
 // }
-{
-  1234
-  // mock
-  const Constructor = Vue.extend(Button);
-  const vm = new Constructor({
-    propsData: {
-      icon: "setting",
-    },
-  });
-  vm.$mount();
-  let spy = chai.spy(function () {});
-  vm.$on("click", spy);
-  let button = vm.$el;
-  button.click();
-  expect(spy).to.have.been.called();
-}
+// {
+//   // mock
+//   const Constructor = Vue.extend(Button);
+//   const vm = new Constructor({
+//     propsData: {
+//       icon: "setting",
+//     },
+//   });
+//   vm.$mount();
+//   let spy = chai.spy(function () {});
+//   vm.$on("click", spy);
+//   let button = vm.$el;
+//   button.click();
+//   expect(spy).to.have.been.called();
+// }
