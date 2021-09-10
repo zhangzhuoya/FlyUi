0cdc1056af829bb339c65b1d933de437599ec755
zhangzhuoya <1114976368@qq.com>
2021-09-10 17:51:02 +0800

    编写input样式

--- src/demo/demo.vue
+++ src/demo/demo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Button type="warning" icon="bianji" iconPosition="left">警告</Button>
+    <!-- <Button type="warning" icon="bianji" iconPosition="left">警告</Button>
     <Button type="danger" icon="bianji" iconPosition="right">危险</Button>
     <Button>默认按钮</Button>
     <Button icon="loading">默认按钮</Button>
@@ -8,17 +8,22 @@
       <Button>上一页</Button>
       <Button>更多</Button>
       <Button>下一页</Button>
-    </button-group>
+    </button-group> -->
+    <Input value="1234" disabled readonly/>
+    <Input value="1234" readonly/>
+    <Input value="1234" error="姓名不能少于5个字"/>
   </div>
 </template>
 
 <script>
 import Button from "@/button.vue";
 import ButtonGroup from "@/buttonGroup.vue";
+import Input from '@/input.vue'
 export default {
   components: {
     Button,
     ButtonGroup,
+    Input
   },
 };
 </script>
---
+++ src/input.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="wrapper" 
+        :class="{'error':error}"
+  >
+      <input 
+        type="text" 
+        :value="value" 
+        :disabled="disabled" 
+        :readonly="readonly"
+        >
+        <template>
+            <icon name="error" class="icon-error"></icon>
+            <span class="error-message">{{error}}</span>
+        </template>
+  </div>
+</template>
+
+<script>
+import Icon from './icon.vue'
+export default {
+    name: 'FInput',
+    components:{
+       Icon
+    },
+    props: {
+        value: {
+            type: String,
+            default: "vv"
+        },
+        disabled:{
+            type: Boolean,
+            default: false
+        },
+        readonly:{
+            type: Boolean,
+            default: false
+        },
+        error: {
+            type: String
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+$height: 36px;
+$border-color: #dcdfe6;
+$border-color-hover: #666;
+$border-radus: 4px;
+$font-size: 14px;
+$box-shadow-color: rgba(0,0,0,0.5);
+$border-focus: #409eff;
+.wrapper {
+    font-size: $font-size;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    > :not(:last-child) {margin-right: 0.5em;}
+    > input {
+        font-size: inherit;
+        height: $height;
+        border: 1px solid $border-color;
+        border-radius: $border-radus;
+        padding: 8px;
+        &:hover {
+            border-color: $border-color-hover;
+        }
+        &:active {
+            // border-color: red;
+        }
+        &:focus {
+            outline: none;
+            border-color: $border-focus;
+        }
+        &[disabled] {
+            border-color: #aaa;
+            color: #aaa;
+            cursor:not-allowed
+        }
+        &[disabled] {
+            border-color: #aaa;
+            color: #aaa;
+            cursor:not-allowed
+        }
+    }
+    &.error {
+      > input { border-color: red; }
+    }
+    .icon-error {
+        fill:red
+    }
+    .error-message {
+        color: red;
+    }
+    
+}
+
+
+</style> 
